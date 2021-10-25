47c0a448f11f55e3537fd0c4126b82842951f58b
Pascal Jacob <poscoljo@aol.com>
2021-10-25 14:58:18 +0200

    Move character with force

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class Character : MonoBehaviour
 {
-    public float Speed = 1;
+    public float Force = 1f;
+    public float MaxSpeed = 2f;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,6 +15,18 @@ public class Character : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        this.transform.position = this.transform.position + new Vector3(this.Speed,0,0);
+        float horizontal = Input.GetAxis("Horizontal");
+
+        Rigidbody2D rigidbody = this.GetComponent<Rigidbody2D>();
+        rigidbody.AddForce(Vector2.right * this.Force * horizontal * Time.deltaTime);
+
+        // Vector2 velocity = rigidbody.velocity;
+        // velocity.x = Mathf.Clamp(velocity.x, -this.MaxSpeed, this.MaxSpeed);
+        // rigidbody.velocity = velocity;
+
+        //Debug.Log(velocity);
+
+        //Add to position to move character
+        //this.transform.position += new Vector3(this.Speed,0f,0f) * horizontal * Time.deltaTime;
     }
 }
