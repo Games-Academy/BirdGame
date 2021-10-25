5c2393c6edcc7a226ce5656c50f7f36eb95d625e
Pascal Jacob <poscoljo@aol.com>
2021-10-25 16:51:28 +0200

    Add jump

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -5,7 +5,9 @@ using UnityEngine;
 public class Character : MonoBehaviour
 {
     public float Force = 1f;
+    public float JumpForce = 1f;
     public float MaxSpeed = 2f;
+    public LayerMask CharacterLayer;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,10 +17,36 @@ public class Character : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //Movement
         float horizontal = Input.GetAxis("Horizontal");
-
         Rigidbody2D rigidbody = this.GetComponent<Rigidbody2D>();
         rigidbody.AddForce(Vector2.right * this.Force * horizontal * Time.deltaTime);
+
+        //Flip
+        Vector3 scale = this.transform.localScale;
+        if (horizontal < 0f)
+        {
+            scale.x = -1.2f;
+        }
+        if(horizontal > 0f)
+        {
+            scale.x = 1.2f;
+        }
+        this.transform.localScale = scale;
+
+        //Animation
+        Animator animator = this.GetComponent<Animator>();
+        float speed = Mathf.Abs(rigidbody.velocity.x);
+        animator.SetFloat("Speed", speed);
+
+        //Jump
+        bool grounded = Physics2D.Raycast(this.transform.position, Vector2.down,1f,~this.CharacterLayer);
+        Debug.Log(grounded);
+        bool jump = Input.GetButtonDown("Jump");
+        if(jump && grounded)
+        {
+            rigidbody.AddForce(Vector2.up * this.JumpForce);
+        }
 
         // Vector2 velocity = rigidbody.velocity;
         // velocity.x = Mathf.Clamp(velocity.x, -this.MaxSpeed, this.MaxSpeed);
