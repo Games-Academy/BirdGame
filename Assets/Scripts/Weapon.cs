9252e8632a387e10c8216253beaacfbb3886380f
Pascal Jacob <poscoljo@aol.com>
2021-11-02 14:43:09 +0100

    Add Weapon functionality

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -5,12 +5,35 @@ using UnityEngine;
 public class Weapon : MonoBehaviour
 {
     public GameObject Bullet;
+    public float BulletSpeed = 1f;
+    public float FireRate = 1f;
+    private float lastFired = 0f;
 
     void Update()
     {
-        if(Input.GetButtonDown("Fire1"))
+        float delay = 1f / this.FireRate;
+        bool canShoot = this.lastFired + delay <= Time.time;
+        
+        if (Input.GetButtonDown("Fire1") && canShoot)
         {
-            Instantiate(this.Bullet,this.transform.position,Quaternion.identity);
+            this.lastFired = Time.time;
+
+            if (this.transform.lossyScale.x > 0f)
+            {
+                GameObject bullet = Instantiate(this.Bullet, this.transform.position, Quaternion.identity);
+                var rigidbody = bullet.GetComponent<Rigidbody2D>();
+                rigidbody.velocity = Vector2.right * this.BulletSpeed;
+            }
+            else
+            {
+                GameObject bullet = Instantiate(this.Bullet, this.transform.position, Quaternion.Euler(0f,0f,180f));
+                var rigidbody = bullet.GetComponent<Rigidbody2D>();
+                rigidbody.velocity = Vector2.left * this.BulletSpeed;
+            }
+
+            //3) Play shoot sound
+            var audioSource = this.GetComponent<AudioSource>();
+            audioSource.Play();
         }
     }
 }
